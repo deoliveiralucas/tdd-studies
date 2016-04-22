f8803c252bde0aa67b3e6c914b06e7531ccf084b
Lucas de Oliveira <94lucasoliveira@bol.com.br>
2016-04-22 20:20:16 -0300

    add mock

--- src/DOLucas/Store/CashFlow/InvoiceGenerator.php
+++ src/DOLucas/Store/CashFlow/InvoiceGenerator.php
@@ -3,17 +3,35 @@
 namespace DOLucas\Store\CashFlow;
 
 use DOLucas\Store\CashFlow\Order;
+use DOLucas\Store\CashFlow\InvoiceDataAccess;
+use DOLucas\Store\CashFlow\SAP;
 use \DateTime;
 
 class InvoiceGenerator
 {
 
+    private $dataAccess;
+    private $sap;
+
+    public function __construct(InvoiceDataAccess $dataAccess, SAP $sap)
+    {
+        $this->dataAccess = $dataAccess;
+        $this->sap = $sap;
+    }
+
     public function generate(Order $order)
     {
-        return new Invoice(
+        $invoice = new Invoice(
             $order->getClient(),
             $order->getTotalValue() * 0.94,
             new DateTime()
         );
+
+        if ($this->dataAccess->persist($invoice) && 
+            $this->sap->send($invoice)) {
+            return $invoice;
+        }
+
+        return null;
     }
 }
--- tests/DOLucas/Store/CashFlow/InvoiceGeneratorTest.php
+++ tests/DOLucas/Store/CashFlow/InvoiceGeneratorTest.php
@@ -5,17 +5,52 @@ namespace DOLucas\Store\CashFlow;
 use DOLucas\Store\Test\TestCase;
 use DOLucas\Store\CashFlow\InvoiceGenerator;
 use DOLucas\Store\CashFlow\Order;
+use Mockery;
 
 class InvoiceGeneratorTest extends TestCase
 {
 
+    protected $generator;
+    protected $dataAccess;
+    protected $sap;
+
+    public function setUp()
+    {
+        $this->dataAccess = Mockery::mock('DOLucas\Store\CashFlow\InvoiceDataAccess');
+        $this->dataAccess->shouldReceive('persist')->andReturn(true);
+
+        $this->sap = Mockery::mock('DOLucas\Store\CashFlow\SAP');
+        $this->sap->shouldReceive('send')->andReturn(true);
+
+        $this->generator = new InvoiceGenerator($this->dataAccess, $this->sap);
+    }
+
     public function testShouldGenerateInvoiceWithTaxValueDiscounted()
     {
-        $generator = new InvoiceGenerator();
         $order = new Order('Lucas', 1000, 1);
 
-        $invoice = $generator->generate($order);
+        $invoice = $this->generator->generate($order);
 
+        $this->assertEquals(1000 * 0.94, $invoice->getValue(), null, 0.00001);
+    }
+
+    public function testShouldPersistGeneratedInvoice()
+    {
+        $order = new Order('Lucas', 1000, 1);
+
+        $invoice = $this->generator->generate($order);
+
+        $this->assertTrue($this->dataAccess->persist($invoice));
+        $this->assertNotNull($invoice);
+    }
+
+    public function testShouldSendGeneratedInvoiceToSAP()
+    {
+        $order = new Order('Lucas', 1000, 1);
+
+        $invoice = $this->generator->generate($order);
+
+        $this->assertTrue($this->sap->send($invoice));
         $this->assertEquals(1000 * 0.94, $invoice->getValue(), null, 0.00001);
     }
 }
